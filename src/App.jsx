c3c1a657a9ed19581c101ddd4dec2ead29725302
Lu <lucasmontenegroburgos@gmail.com>
2024-11-02 16:32:46 -0300

    1° pre-entrega

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,9 @@ import logo from './images/hands-logo.png'; // Asegúrate de tener el logo en la
 function App() {
   return (
     <div>
-      {/* Header con Logo y Bienvenida */}
-      <header className="d-flex justify-content-between align-items-center py-3">
-        <img src={logo} alt="Logo de la tienda" className="cyberpunk-logo" />
-        <h1 className="text-neon">Art-Synt</h1>
-      </header>
       {/* Componente Navbar */}
       <Navbar />
-      <p className="text-neon">Explora lo último en tecnología y estilo Cyberpunk. Productos únicos a un clic de distancia</p>
-      {/* Contenedor de bienvenida */}
+      {/* Contenedor de ítems */}
       <main className="text-center mt-4">
         <ItemListContainer greetings="Bienvenidos" texto="primer-prop" />
       </main>
