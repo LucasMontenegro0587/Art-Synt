bed92b4656f0ea0d6bc40d3e56e641ef5e856bd6
Lu <lucasmontenegroburgos@gmail.com>
2024-11-10 02:01:18 -0300

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './components/Navbar';
-import ItemListContainer from './components/ItemListContainer';
-import Contact from './components/Contact';
+import { NavBar } from './components/NavBar';
+import { ItemListContainer } from './components/ItemListContainer';
+import { Contact } from './components/Contact';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
