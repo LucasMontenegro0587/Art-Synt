978e6637ae1508e42c78beecee7a77e931a4b1d2
Lu <lucasmontenegroburgos@gmail.com>
2024-11-23 21:15:25 -0300

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { NavBar } from './components/NavBar';
 import { ItemListContainer } from './components/ItemListContainer';
-import { ItemDetailContainer } from './components/ItemDetailContainer';
 import { Contact }  from './components/Contact';
 import { Home } from './components/Home';
 import './App.css';
@@ -14,8 +13,7 @@ function App() {
         <NavBar /> {/* Componente Navbar */}
         <Routes>
           <Route path="/Inicio" element={<Home />} />
-          <Route path="/Categorías/:id" element={<ItemListContainer />} />
-          <Route path="/Detalles/:id" element={<ItemDetailContainer />} />
+          <Route path="/Productos" element={<ItemListContainer />} />
           <Route path="/Contacto" element={<Contact />} />
         </Routes>
       </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { CartWidget } from './CartWidget';
-import { HandsLogo } from '../images/hands-logo.png'; // Importa la imagen
+import { handsLogo } from '../images/hands-logo.png'; // Importa la imagen
 
 const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <img src={HandsLogo} alt='main-logo' style={{height: '100px', width:'100px'}} />
+            <img src={handsLogo} alt='main-logo' style={{height: '100px', width:'100px'}} />
             <a className="navbar-brand" href="App.jsx">Inicio</a>
             <div className="collapse navbar-collapse">
                 <ul className="navbar-nav mr-auto">
@@ -13,7 +12,6 @@ const NavBar = () => {
                     <li className="nav-item"><a className="nav-link" href="Contact.jsx">Contacto</a></li>
                 </ul>
             </div>
-            <CartWidget />
         </nav>
     );
 }
